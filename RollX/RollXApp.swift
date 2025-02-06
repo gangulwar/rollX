11d406ebe0ea76af64f699a2721057a66866d8cb
Aarsh Gangulwar <aarshgangulwar23@gmail.com>
2025-02-06 21:04:23 +0530

    Initial Commit

---
+++ RollX/RollXApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollXApp.swift
+//  RollX
+//
+//  Created by Gangulwar on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct RollXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RollXTests/RollXTests.swift
@@ -0,0 +1,17 @@
+//
+//  RollXTests.swift
+//  RollXTests
+//
+//  Created by Gangulwar on 2/6/25.
+//
+
+import Testing
+@testable import RollX
+
+struct RollXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
